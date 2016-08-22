182cc2dd5760702781dd86cda2e813527b82720c
Martijn Senden <martijn@instantmagazine.com>
2016-08-22 17:41:38 +0200

    Expanded comments a bit.

--- getRandomElement.js
+++ getRandomElement.js
@@ -1,7 +1,7 @@
 import getRandomInteger from './getRandomInteger';
 
 /**
- * Get a random Element
+ * Get a random Element from an array
  **/
 export default function getRandomElement (a) {
 	return a[getRandomInteger(0, a.length)];
