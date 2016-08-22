250659368df239e0aa6f0e658b49e1a4ffa0f639
Martijn Senden <martijn@instantmagazine.com>
2016-08-22 11:27:42 +0200

    Added README.md. Renamed getRandomNumber.js to getRandomInteger.js. Added working functions to utility scripts.

--- getRandomElement.js
+++ getRandomElement.js
@@ -1,3 +1,12 @@
-function getRandomElement () {
-  return '<div></div>';
+/**
+ * Get a random Element
+ **/
+function getRandomElement (a) {
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	return a[getRandomInt(0, a.length)];
 }
